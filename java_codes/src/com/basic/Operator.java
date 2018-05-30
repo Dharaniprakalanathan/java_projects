01141d1b10126e99b000cca116afe4cd2d43518a
DharaniPSA <dharaniprakalanathan@gmail.com>
2018-05-30 18:49:52 +0530

    program3

---
+++ java_codes/src/com/basic/Operator.java
@@ -0,0 +1,15 @@
+package com.basic.datatypeexample;
+
+public class Operator {
+	public static void main( String[] args )
+	{
+		int a,b,s;
+		string [] c='lion';
+	    string [] d='cat';
+				a=strlen(c);
+				b=strlen(d);
+				int s[]=(a<b)?a:b;
+				System.out.println(s);
+	}
+
+}
---
+++ java_codes/src/com/basic/WhileLoop2.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class WhileLoop2 {
+	public static void main(String[] args ) {
+		int y=5;
+while (y> 0 && y<=50)
+	{
+	System.out.println( y );
+	++y;
+
+	}
+	}
+}
