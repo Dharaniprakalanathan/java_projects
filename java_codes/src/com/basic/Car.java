678c08ef0b8a4e1180d8f8f05f90c6a4c656a2ee
DharaniPSA <dharaniprakalanathan@gmail.com>
2018-05-31 11:36:43 +0530

    car pro

---
+++ java_codes/src/com/basic/Car.java
@@ -0,0 +1,27 @@
+package com.basic;
+
+public class Car {
+	String model;
+	double speed;
+	public Car()
+	{
+		
+	}
+	public boolean start()
+	{
+		return true;
+	}
+	public void accelerate()
+	{
+		
+	}
+	public static void main(String[] args) {
+		Car car1=new Car();
+		Car car2=new Car();
+		car1.speed=20.0;
+		car2.speed=10.0;
+		System.out.println(car1.speed);
+		System.out.println(car2.speed);
+	}
+
+}
