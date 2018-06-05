073f1171ea3d0e586e26b24136df132820faa424
DharaniPSA <dharaniprakalanathan@gmail.com>
2018-06-05 18:52:50 +0530

    car program 1

--- java_codes/src/com/basic/Car.java
+++ java_codes/src/com/basic/Car.java
@@ -1,27 +1,47 @@
 package com.basic;
 
 public class Car {
-	String model;
-	double speed;
+	private String model;
+	private double speed;
 	public Car()
 	{
 		
 	}
+	
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
 	public boolean start()
 	{
 		return true;
 	}
 	public void accelerate()
 	{
-		
+		speed+=10;
 	}
 	public static void main(String[] args) {
 		Car car1=new Car();
 		Car car2=new Car();
 		car1.speed=20.0;
 		car2.speed=10.0;
+		car1.accelerate();
+		car2.accelerate();
 		System.out.println(car1.speed);
 		System.out.println(car2.speed);
+		
 	}
 
 }
