93aeecfcecb1a7bc04aa46f883603c8dc7a18e99
DharaniPSA <dharaniprakalanathan@gmail.com>
2018-06-05 15:16:45 +0530

    video

---
+++ java_codes/src/com/basic/zz2.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class zz2 {
+	public static void main(String[] args ) {
+		int i = 1,m;
+		while(i<=5)
+		{
+		System.out.println("The :"+i);
+		i++;
+		}			
+			
+		}
+			
+	}
+
