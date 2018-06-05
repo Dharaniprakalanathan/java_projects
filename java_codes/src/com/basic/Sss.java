7a7f2c2ab8c8b7ba4ee92f285354699aec0ac0bc
DharaniPSA <dharaniprakalanathan@gmail.com>
2018-06-05 15:19:57 +0530

    video example

---
+++ java_codes/src/com/basic/Sss.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Sss {
+	public static void main(String[] args ) {
+		int age=0;
+		if ( age >= 65 )
+		System.out.println( "Age is greater than or equal to 65" );
+		else
+		System.out.println( "Age is less than 65 ");
+
+	}
+}
